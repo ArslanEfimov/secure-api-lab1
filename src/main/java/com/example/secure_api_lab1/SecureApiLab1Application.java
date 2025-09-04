01254bf8c03a938c00dfe286e5fc3457b7390549
arslanefimov <dns230717rus@gmail.com>
2025-09-04 11:39:07 +0300

    create project

---
+++ src/main/java/com/example/secure_api_lab1/SecureApiLab1Application.java
@@ -0,0 +1,13 @@
+package com.example.secure_api_lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApiLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApiLab1Application.class, args);
+	}
+
+}
